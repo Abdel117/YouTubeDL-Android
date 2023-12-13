f956a01e2e03fbb22b3d6898bc63951d3e1c18ff
Abdel <114109100+Abdel117@users.noreply.github.com>
2023-12-13 03:06:17 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,2 @@
 from pytube import YouTube
 from io import BytesIO
-
-
-def download_audio(url):
-    try:
-        # Obtén el objeto YouTube
-        yt = YouTube(url)
-
-        # Obtiene la transmisión de audio de la más alta resolución disponible
-        audio_stream = yt.streams.filter(only_audio=True, file_extension="webm").first()
-
-        # Descarga la transmisión de audio en un objeto BytesIO
-        audio_bytesio = BytesIO()
-        audio_stream.stream_to_buffer(audio_bytesio)
-
-        # Vuelve al inicio del objeto BytesIO antes de leer
-        audio_bytesio.seek(0)
-
-        # Lee el contenido del objeto BytesIO (archivo de audio en formato webm)
-        audio_content = audio_bytesio.read()
-        return bytearray(audio_content) #devuelve el contenido en formato de bytearray (para evitar problemas)
-    except Exception as e:
-        return f"Error: {e}"
